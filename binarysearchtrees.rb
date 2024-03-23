4db535d71ca477763be60d9e056cb3222b5084d2
nithindante <Rajkumar_nithin@yahoo.com>
2024-03-24 03:59:12 +0530

    Create all the necessary functions for a Binary Sort tree

--- binarysearchtrees.rb
+++ binarysearchtrees.rb
@@ -18,13 +18,12 @@ class Tree
 
   def initialize(arr = [])
     @arr = arr.sort.uniq
-    @root = build_tree(@arr, 0, arr.length - 1)
+    @root = build_tree(@arr, 0, @arr.length - 1)
   end
 
   def build_tree(arr, start, final)
     mid = (start + final) / 2
     return if start > final
-
     root = Nodes.new(arr[mid])
     root.left = build_tree(arr, start, mid - 1)
     root.right = build_tree(arr, mid + 1, final)
@@ -66,6 +65,7 @@ class Tree
 
   def delete(values, _nodes = @root)
     self.root = delete_at(values, nodes = @root)
+    arr.delete(values)
     self
   end
 
@@ -76,7 +76,14 @@ class Tree
         nodes.right = delete_at(nodes.value, nodes.right)
         return nodes
       elsif !nodes.right.nil?
-        return delete_one_children(values, nodes.right)
+        return delete_one_children(values, nodes.right) if nodes.right.left.nil?
+
+        nodes.value = delete_two_children(values, nodes.right).value
+
+        nodes.right = delete_at(nodes.value, nodes.right)
+
+        return nodes
+
       elsif !nodes.left.nil?
         return delete_one_children(values, nodes.left)
       else
@@ -106,19 +113,188 @@ class Tree
       nodes.left
     end
   end
+
+  def find(values, _nodes = @root)
+    find_at(values, nodes = @root)
+  end
+
+  def find_at(values, nodes = @root)
+    if nodes.value == values
+      return nodes
+    elsif nodes.value < values
+      return find_at(values, nodes.right)
+    elsif nodes.value > values
+      return find_at(values, nodes.left)
+    end
+
+    nodes
+  end
+
+  def level_order(nodes = @root, arr = [])
+        if nodes == nil
+          return
+        end
+        result=[]
+        arr.push(nodes)
+        while arr.length!=0
+          result.push(arr[0].value)
+          if arr[0].left!=nil
+          arr.push(arr[0].left)
+          end
+          if arr[0].right!=nil
+          arr.push(arr[0].right)
+          end
+          arr.shift()
+        end
+      return result
+  end
+
+  def in_order()
+    newArr = in_order_by(nodes=@root)
+    return newArr
+  end
+
+  def in_order_by(nodes=@root,arr =[])
+    temp = nodes
+    if temp == nil
+      return
+    elsif temp.left==nil and temp.right == nil
+      arr.push(temp.value)
+      return temp
+    else
+
+      in_order_by(temp.left,arr)
+      arr.push(temp.value)
+
+      in_order_by(temp.right,arr)
+
+    end
+    return arr
+  end
+
+  def pre_order()
+      pre_order_by(nodes=@root)
+  end
+
+  def pre_order_by(nodes=@root,arr=[])
+    temp = nodes
+      if temp == nil
+        return nil
+      elsif temp.left==nil and temp.right == nil
+        arr.push(temp.value)
+        return temp
+      else
+        arr.push(temp.value)
+        pre_order_by(temp.left,arr)
+        pre_order_by(temp.right,arr)
+      end
+    return arr
+  end
+
+  def post_order()
+      post_order_by(nodes=@root)
+  end
+
+  def post_order_by(nodes=@root,arr=[])
+      if nodes == nil
+        return
+      elsif nodes.left==nil and nodes.right == nil
+        arr.push(nodes.value)
+        return nodes
+      else
+        post_order_by(nodes.left,arr)
+        post_order_by(nodes.right,arr)
+        arr.push(nodes.value)
+      end
+    return arr
+  end
+
+  def height(value=nil)
+      if value == nil
+        return height_of(nodes=@root) - 1
+      else
+      newNode = self.find(value)
+      return height_of(nodes=newNode) - 1
+      end
+  end
+
+  def height_of(nodes)
+      if nodes == nil
+        return 0
+      else
+        leftHeight = height_of(nodes.left)
+        rightHeight = height_of(nodes.right)
+        if leftHeight > rightHeight
+          return leftHeight + 1
+        else
+          return rightHeight + 1
+        end
+      end
+  end
+
+  def depth(values=nil,nodes=@root)
+    if values == nil
+      return 0
+    else
+    depth_of(values,nodes=@root)
+    end
+  end
+
+  def depth_of(values,nodes=@root)
+    if values == nodes.value
+        return 0
+    else
+      if(values>nodes.value)
+          return depth_of(values,nodes.right) + 1
+      elsif(values<nodes.value)
+         return  depth_of(values,nodes.left)  + 1
+      end
+    end
+  end
+
+  def balanced(nodes=@root)
+    if nodes.left == nil or nodes.right == nil
+      p "balanced"
+      return
+    end
+    if (height(nodes.left.value)==height(nodes.right.value))
+      p " balanced"
+    else
+      p "not balanced"
+    end
+  end
+
+  def rebalance
+    self.root.value = inorder_array
+    self.root = build_tree(arr,0,inorder_array.length - 1)
+  end
+
+  def inorder_array(node = @root, array = [])
+    unless node.nil?
+      inorder_array(node.left, array)
+      array << node.value
+      inorder_array(node.right, array)
+    end
+    array
+  end
+
 end
 
-n = Nodes.new(5)
-nn = Nodes.new(6)
+t = Tree.new((Array.new(15) {rand(1..100) }))
+ t.pretty_print
+ t.balanced()
+p t.level_order
+p  t.pre_order
+p t.post_order
+  p t.in_order
 
-t = Tree.new([1, 2, 3, 4, 10, 13, 5])
-t.pretty_print
-t.insert(6)
-t.insert(15)
-t.insert(12)
-t.insert(11)
-t.insert(14)
-t.insert(16)
-t.pretty_print
-t.delete(13)
-t.pretty_print
+  t.insert(117)
+ t.pretty_print
+t.balanced()
+ t.rebalance()
+ t.pretty_print
+t.balanced()
+p t.level_order
+p  t.pre_order
+p t.post_order
+  p t.in_order
